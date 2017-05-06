0ffeace791cac5765b665679c4e25a107681ea4a
natesampo <nathan.sampo@students.olin.edu>
2017-05-06 19:58:25 -0400

    fixed up movement code

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -1,23 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class FirstPersonController : MonoBehaviour {
-
-    public float movementspeed = 5.0f;
-    public float mousesensitivity = 5.0f;
-
-    float vertrot = 0;
+public class FirstPersonController : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    public float mousesensitivity = 5.0F;
     public float updownrange = 60.0f;
+    float vertrot = 0.0F;
+    private Vector3 moveDirection = Vector3.zero;
 
-	// Use this for initialization
-	void Start () {
-        Screen.lockCursor = true; // lock mouse to the screen (removes mouse from view and keeps it in screen center)
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        // Rotate Left and Right based on mouse position
+    private void Start()
+    {
+        Screen.lockCursor = true;
+    } 
+
+    void Update()
+    {
         float leftrightrot = Input.GetAxis("Mouse X") * mousesensitivity;
         transform.Rotate(0, leftrightrot, 0);
 
@@ -25,15 +25,16 @@ public class FirstPersonController : MonoBehaviour {
         vertrot -= Input.GetAxis("Mouse Y") * mousesensitivity; //minus because updown rot is backwards
         vertrot = Mathf.Clamp(vertrot, -updownrange, updownrange);
         Camera.main.transform.rotation = Quaternion.Euler(vertrot, transform.rotation.eulerAngles.y, 0);
-
-        // Move Based On WASD or ARROWKEYS
-        float sidespeed = Input.GetAxis("Horizontal") * movementspeed;
-        float forwardspeed = Input.GetAxis("Vertical") * movementspeed;
-        Vector3 velocity = new Vector3(sidespeed, 0, forwardspeed);
-
-        velocity = transform.rotation * velocity;
-        //Get Character Controller and execute Simple Movement Script
-        CharacterController cc = GetComponent<CharacterController>();
-        cc.Move(velocity*Time.deltaTime);
-	}
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        moveDirection = transform.TransformDirection(moveDirection);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
 }
