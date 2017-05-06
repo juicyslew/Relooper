7cf54c1c3c1d733f287dcfbc4401e14a147f6c88
William Derksen <wderksen@olin.edu>
2017-05-06 16:39:07 -0400

    Added Basic Forwards and Backwards Movement

---
+++ Assets/FirstPersonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour {
+
+    public float movementspeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float forwardspeed = Input.GetAxis("Vertical")*movementspeed;
+        Vector3 velocity = new Vector3(0, 0, forwardspeed);
+        CharacterController cc = GetComponent<CharacterController>();
+        cc.SimpleMove(velocity);
+	}
+}
