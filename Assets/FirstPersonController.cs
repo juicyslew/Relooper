d8b67ae572f0165b59941a2ac3840c960d3942b5
William Derksen <wderksen@olin.edu>
2017-05-06 18:52:14 -0400

    Added Movement and Rotation based on mouse

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class FirstPersonController : MonoBehaviour {
 
     public float movementspeed = 5.0f;
+    public float mousesensitivity = 5.0f;
+
+    float vertrot = 0;
+    public float updownrange = 60.0f;
 
 	// Use this for initialization
 	void Start () {
-		
+        Screen.lockCursor = true; // lock mouse to the screen (removes mouse from view and keeps it in screen center)
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float forwardspeed = Input.GetAxis("Vertical")*movementspeed;
-        Vector3 velocity = new Vector3(0, 0, forwardspeed);
+        // Rotate Left and Right based on mouse position
+        float leftrightrot = Input.GetAxis("Mouse X") * mousesensitivity;
+        transform.Rotate(0, leftrightrot, 0);
+
+        //rotate Up and down the camera based on mouse position
+        vertrot -= Input.GetAxis("Mouse Y") * mousesensitivity; //minus because updown rot is backwards
+        vertrot = Mathf.Clamp(vertrot, -updownrange, updownrange);
+        Camera.main.transform.rotation = Quaternion.Euler(vertrot, transform.rotation.eulerAngles.y, 0);
+
+        // Move Based On WASD or ARROWKEYS
+        float sidespeed = Input.GetAxis("Horizontal") * movementspeed;
+        float forwardspeed = Input.GetAxis("Vertical") * movementspeed;
+        Vector3 velocity = new Vector3(sidespeed, 0, forwardspeed);
+
+        velocity = transform.rotation * velocity;
+        //Get Character Controller and execute Simple Movement Script
         CharacterController cc = GetComponent<CharacterController>();
         cc.SimpleMove(velocity);
 	}
