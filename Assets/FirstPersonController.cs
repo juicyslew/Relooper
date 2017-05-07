776754e50145b96012e93a17a8f96fa6fc80bc04
natesampo <nathan.sampo@students.olin.edu>
2017-05-06 20:32:06 -0400

    Fixed jumping while moving

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -30,11 +30,11 @@ public class FirstPersonController : MonoBehaviour
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection *= speed;
+            moveDirection = transform.TransformDirection(moveDirection);
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
         }
         moveDirection.y -= gravity * Time.deltaTime;
-        moveDirection = transform.TransformDirection(moveDirection);
         controller.Move(moveDirection * Time.deltaTime);
     }
 }
