72779a0d75ba4faa23394d70326536183ed4b231
William Derksen <wderksen@olin.edu>
2017-05-06 20:58:53 -0400

    fixed thing

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -31,7 +31,6 @@ public class FirstPersonController : MonoBehaviour
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
-            moveDirection = transform.TransformDirection(moveDirection);
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
         }
