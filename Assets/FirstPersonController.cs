cc89f7a7114a61487ee2187d196aa0de8b2654dd
natesampo <nathan.sampo@students.olin.edu>
2017-05-07 03:53:43 -0400

    Added functioning Gun and Bullets

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -8,13 +8,16 @@ public class FirstPersonController : MonoBehaviour
     public float gravity = 20.0F;
     public float mousesensitivity = 5.0F;
     public float updownrange = 60.0f;
+    public float bulletSpeed = 50.0f;
     float vertrot = 0.0F;
     private Vector3 moveDirection = Vector3.zero;
+    public GameObject BulletPrefab;
+    public Transform BulletSpawn;
 
     private void Start()
     {
         Screen.lockCursor = true;
-    } 
+    }
 
     void Update()
     {
@@ -36,5 +39,21 @@ public class FirstPersonController : MonoBehaviour
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = Instantiate(BulletPrefab, BulletSpawn.position, BulletSpawn.rotation);
+
+        // Add velocity to the Bullet
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward*bulletSpeed;
+
+        // Destroy the Bullet after 2 seconds
+        Destroy(bullet, 1.5f);
     }
 }
