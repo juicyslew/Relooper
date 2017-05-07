ef676025018bb640a0277d56f64ca4a1f51e53f5
William Derksen <wderksen@olin.edu>
2017-05-06 20:57:40 -0400

    Fixed Air Spinning, Added Some Ramps to walk on

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -29,6 +29,7 @@ public class FirstPersonController : MonoBehaviour
         if (controller.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
             moveDirection = transform.TransformDirection(moveDirection);
             if (Input.GetButton("Jump"))
